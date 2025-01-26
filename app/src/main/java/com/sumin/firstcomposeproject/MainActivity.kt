b16d108e600b8d1667f0974e28adedcb97fb813b
Ivan Toropygin <itori86@mail.ru>
2025-01-26 11:49:28 +0300

    lesson_5 addCard for border

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -3,17 +3,12 @@ package com.sumin.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,42 +19,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            TimesTable()
             InstagramProfileCard()
         }
     }
 }
 
 @Preview
-
 @Composable
-fun TimesTable() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    )
-    {
-        for (i in 1..9) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                for (j in 1..9) {
-                    val backgroundColor =
-                        if ((i + j) % 2 == 0) Color.Yellow else Color.White
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, color = Color.Black)
-                            .background(color = backgroundColor),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text("${i * j}")
-                    }
-                }
-            }
-        }
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(Color.Green)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(8.dp),
+            text = "Hello world!",
+            color = Color.White
+        )
     }
 }

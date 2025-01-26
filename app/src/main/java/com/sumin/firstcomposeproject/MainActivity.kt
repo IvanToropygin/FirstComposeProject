b8b1f83c7739dd5b68b0ed5ecb6fcc73de351a84
Ivan Toropygin <itori86@mail.ru>
2025-01-26 15:15:46 +0300

    lesson_7 icon

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -3,11 +3,15 @@ package com.sumin.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.sumin.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.sumin.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -24,4 +28,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun ImageTest() {
+    Image(painter = painterResource(R.drawable.ic_instagram),
+        contentDescription = "Instagram icon")
+
 }
--- app/src/main/java/com/sumin/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,15 +1,15 @@
 package com.sumin.firstcomposeproject.ui.theme
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -18,13 +18,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sumin.firstcomposeproject.R
 
 @Composable
 fun InstagramProfileCard() {
@@ -42,17 +43,17 @@ fun InstagramProfileCard() {
             horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Box(
+            Image(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(color = Color.Yellow)
-            ) {
-
-            }
+                    .clip(CircleShape)
+                    .size(50.dp),
+                painter = painterResource(R.drawable.ic_instagram),
+                contentDescription = "Instagram icon"
+            )
 
             UserStatistics(title = "Posts", value = "6,950")
             UserStatistics(title = "Followers", value = "436M")
-            UserStatistics(title = "Following", value =  "76")
+            UserStatistics(title = "Following", value = "76")
         }
     }
 }
@@ -60,7 +61,7 @@ fun InstagramProfileCard() {
 @Composable
 private fun UserStatistics(
     title: String,
-    value: String
+    value: String,
 ) {
     Column(
         modifier = Modifier
